dced902bcd2b1e44e976d8f7dbc14fd813b1222c
yaodong <wangyaodong0126@163.com>
2017-07-18 17:57:37 +0800

    yaodong :: add menu component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <nav class="bar bar-tab">
-      <a class="tab-item external active" href="#">
-          <span class="icon icon-home"></span>
-          <span class="tab-label">首页</span>
-      </a>
-      <a class="tab-item external" href="#">
-          <span class="icon icon-star"></span>
-          <span class="tab-label">收藏</span>
-      </a>
-      <a class="tab-item external" href="#">
-          <span class="icon icon-settings"></span>
-          <span class="tab-label">设置</span>
-      </a>
-  </nav>
+    <footerMenu></footerMenu>
   </div>
 </template>
 
@@ -25,27 +12,16 @@ export default {
     return {
       msg: 'My Music Player'
     }
+  },
+  components: {
+    footerMenu: require('./menu.vue')
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
---
+++ src/components/menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="bar bar-tab">
+      <a class="tab-item external active" href="#">
+          <span class="icon icon-home"></span>
+          <span class="tab-label">发现音乐</span>
+      </a>
+      <a class="tab-item external" href="#">
+          <span class="icon icon-star"></span>
+          <span class="tab-label">我的音乐</span>
+      </a>
+      <a class="tab-item external" href="#">
+          <span class="icon icon-settings"></span>
+          <span class="tab-label">设置</span>
+      </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'menu'
+}
+</script>
+
